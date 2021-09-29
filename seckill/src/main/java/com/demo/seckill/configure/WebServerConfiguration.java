a7696554f85b5a0ddf36b601279d69383b67ca7e
yzhong0502 <yuanz0502@gmail.com>
2021-09-29 17:26:38 -0500

    added tomcat config

---
+++ seckill/src/main/java/com/demo/seckill/configure/WebServerConfiguration.java
@@ -0,0 +1,30 @@
+package com.demo.seckill.configure;
+
+
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11Nio2Protocol;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+//当spring容器内没有TomcatEmbeddedServletContainerFactory这个bean时，会把此bean加载进spring容器中
+@Component
+public class WebServerConfiguration implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        ((TomcatServletWebServerFactory)factory).addConnectorCustomizers(new TomcatConnectorCustomizer() {
+            @Override
+            public void customize(Connector connector) {
+                Http11Nio2Protocol protocol = (Http11Nio2Protocol) connector.getProtocolHandler();
+                //定制化keepalivetimeout,30s内没有请求则自动断开连接
+                protocol.setKeepAliveTimeout(30000);
+                //当客户端发送超过10000个请求则自动断开连接
+                protocol.setMaxKeepAliveRequests(10000);
+            }
+        });
+    }
+}
